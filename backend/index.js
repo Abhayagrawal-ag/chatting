136c150861ef581825add06d3e0b288d0b6e09e9
Abhayagrawal-ag <your.email@example.com>
2025-07-13 14:46:04 +0530

    all changes

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,12 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors("https://chatting-frontend-j6ta.onrender.com"));
 const PORT = process.env.PORT || 3000;
 
 
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
-// // Serve static files from React dist
-// app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
 
 
 const server = http.createServer(app);
